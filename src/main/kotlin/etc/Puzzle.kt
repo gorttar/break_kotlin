8c5031caac7518b2b5497f18c09023466f6ddae8
Andrey Antipov <andrey.antipov@cxense.com>
2019-02-26 19:21:01 +0400

    add lambda puzzle

---
+++ src/main/kotlin/etc/Puzzle.kt
@@ -0,0 +1,11 @@
+package etc
+
+/**
+ * etc/Puzzle.kt
+ */
+fun main() = println("Hello, human")
+    .fun Any.() { println("Kotlin") }()
+    .fun Any.() { println("Will") }()
+    .fun Any.() { println("Rule") }()
+    .fun Any.() { println("The") }()
+    .fun Any.() { println("World") }()
