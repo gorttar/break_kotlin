83aac402528fd55fd9d071ec9972605d7ae7d3ad
Andrey Antipov <andrey.antipov@cxense.com>
2019-02-27 13:52:16 +0400

    simplify alias bug example

--- src/main/kotlin/alias/AliasBug.kt
+++ src/main/kotlin/alias/AliasBug.kt
@@ -4,22 +4,14 @@ package alias
  * alias/AliasBug.kt
  */
 class Foo {
-    companion object {
-        val foo = "foo"
-    }
-
-    object Bar {
-        val bar = "bar"
-    }
+    object Bar
 }
 
 typealias Baz = Foo
 
 fun main() {
     // компилируется
-    println(Foo.foo)
-    println(Baz.foo)
-    println(Foo.Bar.bar)
+    println(Foo.Bar)
     // не компилируется с Unresolved reference: Bar
-//    println(Baz.Bar.bar)
+//    println(Baz.Bar)
 }
